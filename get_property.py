4b5143ba1dc6ca9db5f1ea1b4b6780158ba24ec8
Jeff Schuler <jeff@websubstrate.com>
2013-07-04 15:55:30 -0400

    Start working with cookies in get_property to try to handle __doPostBack.

--- get_property.py
+++ get_property.py
@@ -4,6 +4,9 @@ import urllib2
 import urllib
 import logging
 import logging.handlers
+import cookielib
+
+import pprint
 
 # Logging
 global _LOG_FILEPATH
@@ -13,11 +16,11 @@ global _logger
 _logger = logging.getLogger('Logger')
 
 # Log to console
-consoleHandler = logging.StreamHandler()
-consoleHandler.setLevel(logging.DEBUG)
-consoleFormatter = logging.Formatter("%(message)s")
-consoleHandler.setFormatter(consoleFormatter)
-_logger.addHandler(consoleHandler)
+#consoleHandler = logging.StreamHandler()
+#consoleHandler.setLevel(logging.DEBUG)
+#consoleFormatter = logging.Formatter("%(message)s")
+#consoleHandler.setFormatter(consoleFormatter)
+#_logger.addHandler(consoleHandler)
 
 # Log to logfile
 _logger.setLevel(logging.DEBUG)
@@ -26,20 +29,51 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 _logger.addHandler(handler)
 
+# Pretty-Printer
+pp = pprint.PrettyPrinter(indent=2)
 
 def get_parcel_info(parcel_num):
     _logger.debug('getting parcel #' + parcel_num)
-    formUrl = "http://sheriff.cuyahogacounty.us/en-US/Foreclosure-Property-Search.aspx"
-    formValues = {'ctl00_ContentPlaceHolder1_txtParcelNumber': parcel_num}
-    formData = urllib.urlencode(formValues)
-    formRequest = urllib2.Request(formUrl, formData)
-    formRequestResponse = urllib2.urlopen(formRequest)
-    formRequestResponseHtml = formRequestResponse.read()
-    return formRequestResponseHtml
+
+    base_url = 'http://sheriff.cuyahogacounty.us/en-US'
+    page_url = base_url + '/Foreclosure-Property-Search.aspx'
+    form_url = base_url + '/PropertySearch.aspx?LanguageCD=en-US&ItemKey=26945&pb=y'
+
+    jar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
+    urllib2.install_opener(opener)
+    #r = opener.open(base_url)
+    urllib2.urlopen(page_url)
+    pp.pprint(jar)
+
+    #form_values = {'ctl00_ContentPlaceHolder1_txtParcelNumber': parcel_num}
+    form_values = {
+      'ctl00$ctl04': 'ctl00$ContentPlaceHolder1$UpdatePanel1|ctl00$ContentPlaceHolder1$btnSearchForeclosureProperties',
+      'ctl00$ContentPlaceHolder1$txtParcelNumber': parcel_num,
+      'ctl00$ContentPlaceHolder1$TextBoxWatermarkExtender1_ClientState': None,
+      'ctl00$ContentPlaceHolder1$MaskedEditExtenderParcelNumber_ClientState': None,
+      'ctl00$ContentPlaceHolder1$ddlCity': None,
+      'ctl00$ctl11': 'nnennnn'
+      }
+    form_data = urllib.urlencode(form_values)
+
+    form_request = urllib2.Request(form_url, form_data)
+    form_response = urllib2.urlopen(form_request)
+    response_html = form_response.read()
+
+    outfile_path = 'data/' + parcel_num + '.html'
+    outfile = file(outfile_path, 'w')
+    outfile.write(response_html)
+    outfile.close()
+
+    return outfile_path
 
 # @TODO:
-# Parse "Number of Properties Found: \d"
+# Parse return:
+#   #ctl00_ContentPlaceHolder1_pnlResultPanel
 # get table
+# "Number of Properties Found: \d"
+#   #ctl00_ContentPlaceHolder1_lblNumberOfRecordsFound
 # parse Sale Date, Sale Number, Case Number Status
 # click View
 # get other details
